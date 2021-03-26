cf387681ae30fee0966197be2e40641eb78be3d1
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-03-26 14:20:05 -0700

    import Header into app

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
-  <amplify-authenticator>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <amplify-sign-out></amplify-sign-out>
-  </amplify-authenticator>
-  <router-view />
+  <div id="app">
+    <TheHeader />
+    <amplify-authenticator>
+      
+    </amplify-authenticator>
+    <router-view />
+  </div>
 </template>
 
+<script >
+import { defineComponent } from "vue";
+import TheHeader from "./components/TheHeader";
+export default defineComponent({
+  components: {
+    TheHeader,
+  },
+});
+</script>
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  max-width: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 100%;
+  font-size: 62.5%;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
