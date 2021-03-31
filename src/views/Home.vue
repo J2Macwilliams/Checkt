11737884764812765831f9b18a9802f268628d8e
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-03-30 17:16:04 -0700

    change imports for TS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="home">
+  <div class="flex flex-col items-center h-screen">
+    <AddItem />
     <ListItems />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
 import ListItems from '@/components/ListItems.vue';
+import AddItem from '@/components/AddItem.vue'
 
 export default defineComponent({
   name: "Home",
   components: {
     ListItems,
+    AddItem
   },
+ 
 });
 </script>
+
