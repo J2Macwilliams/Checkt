1304499b48a84c76416945ea7866ab437a1853fe
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-03-30 17:14:53 -0700

    changee styling for listItems

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="list-items">
-    <div :key="item.id" v-for="item in items" class="item">
-      <h1 class="item-name">{{ item.name }}</h1>
+  <div class="w-5/6 grid grid-cols-3 grid-flow-row border-2 rounded-lg">
+    <div :key="item.id" v-for="item in allItems" class="text-xl flex items-center justify-between p-2 m-2 border-2 bg-green-600 rounded-lg text-white">
+      <h1 >
+        {{ item.name }}
+      </h1>
+      <h2>{{item.store}}</h2>
+      <button >X</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapGetters, mapActions } from "vuex";
+
 
 export default defineComponent({
   name: "ListItems",
-  data() {
-    return {
-      items: [{ id: 1, name: "avocado" }],
-    };
+  
+  methods: {
+    ...mapActions(["fetchItems"]),
+  },
+  computed: mapGetters(["allItems"]),
+  created() {
+    this.fetchItems();
+    
   },
 });
 </script>
 
-<style scoped>
-.list-items {
-  display: grid;
-}
-.item {
-}
-
-.item-name {
-  font-size: 1.5rem;
-}
-</style>
