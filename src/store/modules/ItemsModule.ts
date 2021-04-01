850dfcf52787c29e6694677a85638942bc3d1e12
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-04-01 12:57:21 -0700

    add logic to ItemsModule vuex

--- src/store/modules/ItemsModule.ts
+++ src/store/modules/ItemsModule.ts
@@ -1,30 +1,30 @@
 import { API } from 'aws-amplify'
 import { State, Item } from '../interfaces'
-import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { onAuthUIStateChange } from "@aws-amplify/ui-components";
 
 const state = {
   items: [],
-  user: {
-    attributes: {
-      sub: ''
-    }
-  }
+  user: undefined,
+  authState: undefined,
+  unsubscribeAuth: undefined,
+  isNavOpen: false
 }
 
 const getters = {
   getUser: (state: State) => state.user,
+  getNavStatus: (state: State) => state.isNavOpen,
+  getAuthState: (state: State) => state.authState,
   allItems: (state: State) => state.items,
 
 }
 
 const actions = {
-  async fetchUser({commit}: {commit: Function}) {
-    onAuthUIStateChange((state, user) => {
-      if (state === AuthState.SignedIn) {
-        console.log(`user`, user)
-        commit('setUser', user)
-      }
-    });
+  
+  triggerAuth({ commit }: { commit: Function }) {
+    commit('setAuth')
+  },
+  clearAuth({commit}: {commit: Function}){
+    commit('cleanSlate')
   },
   async fetchItems({ commit }: { commit: Function }) {
     const response = await API.get('checktapi', '/items/userId', {})
@@ -32,20 +32,25 @@ const actions = {
     commit('setItems', response)
   },
   async addItem({ commit }: { commit: Function }, data: Item) {
-    
+
     await API.post('checktapi', '/items', {
       body: {
         ...data,
-        
+
       }
     })
-    
+
     commit('newItem', data)
   }
 }
 
 const mutations = {
-  setUser: (state: State, user: any) => (state.user = user),
+  toggleNav: (state: State) => (state.isNavOpen = !state.isNavOpen),
+  setAuth: (state: State) => (state.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+    state.authState = authState;
+    state.user = authData;
+  })),
+  cleanSlate: (state: State) => state.unsubscribeAuth(),
   setItems: (state: State, items: Item[]) => (state.items = items),
   newItem: (state: State, item: Item) => (state.items.unshift(item))
 }
