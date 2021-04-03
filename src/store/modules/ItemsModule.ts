f3c5c9981198a98851ba77e38900926cb0c1360c
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-04-02 17:09:19 -0700

    add to ItemsModule vuex

--- src/store/modules/ItemsModule.ts
+++ src/store/modules/ItemsModule.ts
@@ -1,9 +1,10 @@
 import { API } from 'aws-amplify'
-import { State, Item } from '../interfaces'
+import { State, Product, Store } from '../interfaces'
 import { onAuthUIStateChange } from "@aws-amplify/ui-components";
 
 const state = {
   items: [],
+  stores: [],
   user: undefined,
   authState: undefined,
   unsubscribeAuth: undefined,
@@ -15,32 +16,35 @@ const getters = {
   getNavStatus: (state: State) => state.isNavOpen,
   getAuthState: (state: State) => state.authState,
   allItems: (state: State) => state.items,
-
+  allStores: (state: State) => state.stores
 }
 
 const actions = {
-  
-  triggerAuth({ commit }: { commit: Function }) {
-    commit('setAuth')
-  },
-  clearAuth({commit}: {commit: Function}){
-    commit('cleanSlate')
-  },
   async fetchItems({ commit }: { commit: Function }) {
     const response = await API.get('checktapi', '/items/userId', {})
     console.log('fetchItems', response)
     commit('setItems', response)
   },
-  async addItem({ commit }: { commit: Function }, data: Item) {
-
-    await API.post('checktapi', '/items', {
-      body: {
-        ...data,
-
-      }
-    })
-
+  async addItem({ commit }: { commit: Function }, data: Product) {
+    await API.post('checktapi', '/items', { body: { ...data, } })
     commit('newItem', data)
+  },
+  async fetchStores({ commit }: { commit: Function }) {
+    const response = await API.get('checktapi', '/stores/userId', {})
+    console.log('fetchStores', response)
+    commit('setStores', response)
+  },
+  async addStore({ commit }: { commit: Function }, data: Store) {
+    await API.post('checktapi', '/stores', { body: { ...data, } })
+    commit('newStore', data)
+  },
+  async deleteItem({ commit }: { commit: Function }, id: string) {
+    await API.del('checktapi', `/items/object/id/${id}`, {})
+    commit('removeItem', id)
+  },
+  async deleteStore({ commit }: { commit: Function }, id: string) {
+    await API.del('checktapi', `/stores/object/id/${id}`, {})
+    commit('removeStore', id)
   }
 }
 
@@ -50,9 +54,14 @@ const mutations = {
     state.authState = authState;
     state.user = authData;
   })),
-  cleanSlate: (state: State) => state.unsubscribeAuth(),
-  setItems: (state: State, items: Item[]) => (state.items = items),
-  newItem: (state: State, item: Item) => (state.items.unshift(item))
+  clearAuth: (state: State) => state.unsubscribeAuth(),
+  // coming from actions
+  setItems: (state: State, items: Product[]) => (state.items = items),
+  newItem: (state: State, item: Product) => (state.items.unshift(item)),
+  setStores: (state: State, stores: Store[]) => (state.stores = stores),
+  newStore: (state: State, store: Store) => (state.stores.unshift(store)),
+  removeItem: (state: State, id: string) => (state.items = state.items.filter(item => item.id !== id)),
+  removeStore: (state: State, id: string) => (state.stores = state.stores.filter(store => store.id !== id)),
 }
 
 export default { state, getters, actions, mutations }
