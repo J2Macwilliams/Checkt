0ac825c39c233dc084afdac1b96fe6d6e7eb88cd
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-04-05 15:45:53 -0700

    add style and pass toggleBought fn to Item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,23 +1,29 @@
 <template>
   <div
-    class="flex items-center justify-between m-1 p-2 border-2 border-gray-600  rounded-lg text-xl hover:transform scale-50"
+    class="flex items-center justify-between m-3 p-2 border-2 border-gray-600 rounded-lg text-xl transition transform shadow-lg"
     :style="{ background: item.store.color }"
+    :class="{ 'scale-90 shadow-none border-dashed ': item.bought }"
+    @dblclick="toggleBought(item.id)"
   >
-    <p >{{ item.qty }}</p>
-    <p >
+    <p>{{ item.qty }}</p>
+    <p :class="{ 'line-through': item.bought }">
       {{ item.name }}
     </p>
-    <i class="fas fa-times" @click.prevent="deleteItem(item.id)"></i>
-    </div>
+    <i
+      class="fas fa-times transform transition hover:scale-125"
+      @click.prevent="deleteItem(item.id)"
+    ></i>
+  </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapMutations } from "vuex";
 export default {
   name: "Item",
   props: ["item"],
   methods: {
     ...mapActions(["deleteItem"]),
+    ...mapMutations(["toggleBought"]),
   },
 };
 </script>
